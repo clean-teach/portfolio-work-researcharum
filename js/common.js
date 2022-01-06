576d19fa7efd453268a60a92f50128ae4312cd3b
kch <kch19920726@gmail.com>
2022-01-06 17:53:52 +0900

    main page 본문 영역 1차 완성
    - 메뉴 모션 작업 필요

--- js/common.js
+++ js/common.js
@@ -1,7 +1,9 @@
 (function(){
     const header = document.querySelector('header');
     const mainDropdownMenu = document.querySelector('.depth-sub');
+    const mainDropdownMenuH = mainDropdownMenu.offsetHeight;
     const searchArea = document.querySelector('#header-search-area');
+    console.log(mainDropdownMenuH);
 
     // setDimmed(header);
     // function setDimmed(target){
@@ -17,12 +19,14 @@
     function showDropdownMenu(dropdownMenu){
         header.classList.add('on');
         dropdownMenu.style.display = 'block';
+        dropdownMenu.classList.add('on');
         // dropdownMenu.style.transform = 'scaleY(1)';
         // dropdownMenu.style.transition = '.4s';
     }
     function hideDropdownMenu(dropdownMenu){
         header.classList.remove('on');
         dropdownMenu.style.display = 'none';
+        dropdownMenu.classList.remove('on');
         // dropdownMenu.style.transform = 'scaleY(0)';
     }
 
@@ -33,8 +37,23 @@
         hideDropdownMenu(mainDropdownMenu);
     });
 
-    document.querySelector('#btn-search-toggle').addEventListener('click', function(){
-        searchArea.classList.toggle('on');
+    function showSearchArea(searchArea){
+        document.body.style.overflow = "hidden";
+        searchArea.classList.add('on');
         hideDropdownMenu(mainDropdownMenu);
+    }
+    function hideSearchArea(searchArea){
+        document.body.style.overflow = "scroll";
+        searchArea.classList.remove('on');
+    }
+
+    document.querySelector('#btn-search-toggle').addEventListener('click', function(){
+        console.log(searchArea.classList.value === 'on');
+        console.log(searchArea.classList.value);
+        if(searchArea.classList.value === 'on'){
+            hideSearchArea(searchArea);
+        }else{
+            showSearchArea(searchArea);
+        }
     });
 }())
