a4ba8888fd56062c9550ffeff226aa3db48a4d56
kch <kch19920726@gmail.com>
2022-01-07 14:20:44 +0900

    메인 메뉴 모션 추가 등
    - "투자 아이디어 공유" 페이지 게시판 내용 수정
    - 전체 서브페이지 css 선택자 수정(h2 -> section h2)

--- js/common.js
+++ js/common.js
@@ -1,9 +1,22 @@
+// Utils
+let scrollBaseValue = 0;
+function getScrollDirection(){
+    let result;
+    if(scrollBaseValue < window.scrollY) result = 'DOWN';
+    else result = 'UP';
+    scrollBaseValue = window.scrollY;
+    return result;
+}
+
 (function(){
     const header = document.querySelector('header');
+    const btnMainMenu = document.querySelector('#btn-main-menu');
+    const gnbBtns = document.querySelector('header .gnb').querySelectorAll('a');
     const mainDropdownMenu = document.querySelector('.depth-sub');
     const mainDropdownMenuH = mainDropdownMenu.offsetHeight;
+    const mainDropdownMenuDepth01List = mainDropdownMenu.querySelectorAll('.depth-01>li');
+    const mainDropdownMenuBtns = mainDropdownMenu.querySelectorAll('a');
     const searchArea = document.querySelector('#header-search-area');
-    console.log(mainDropdownMenuH);
 
     // setDimmed(header);
     // function setDimmed(target){
@@ -18,24 +31,28 @@
 
     function showDropdownMenu(dropdownMenu){
         header.classList.add('on');
-        dropdownMenu.style.display = 'block';
+        dropdownMenu.style.transition = .4 + 's';
+        dropdownMenu.style.height = mainDropdownMenuH + 'px';
+        mainDropdownMenuBtns.forEach(btn=>{
+            btn.style.transition = .4 + 's';
+            btn.style.transitionDelay = .1 + 's';
+            btn.style.opacity = 1;
+        });
         dropdownMenu.classList.add('on');
-        // dropdownMenu.style.transform = 'scaleY(1)';
-        // dropdownMenu.style.transition = '.4s';
+        btnMainMenu.classList.add('close');
     }
     function hideDropdownMenu(dropdownMenu){
         header.classList.remove('on');
-        dropdownMenu.style.display = 'none';
+        dropdownMenu.style.height = 0;
+        mainDropdownMenuBtns.forEach(btn=>{
+            btn.style.opacity = 0;
+        });
+        gnbBtns.forEach((btn, i, arr) => {
+            btn.classList.remove('on');
+        });
         dropdownMenu.classList.remove('on');
-        // dropdownMenu.style.transform = 'scaleY(0)';
+        btnMainMenu.classList.remove('close');
     }
-
-    header.querySelector('.gnb').addEventListener('mouseenter', function(){
-        showDropdownMenu(mainDropdownMenu);
-    });
-    mainDropdownMenu.querySelector('.depth-01').addEventListener('mouseleave', function(){
-        hideDropdownMenu(mainDropdownMenu);
-    });
 
     function showSearchArea(searchArea){
         document.body.style.overflow = "hidden";
@@ -47,13 +64,72 @@
         searchArea.classList.remove('on');
     }
 
+    hideDropdownMenu(mainDropdownMenu);
+    header.querySelector('.gnb').addEventListener('mouseenter', function(){
+        showDropdownMenu(mainDropdownMenu);
+    });
+    mainDropdownMenu.querySelector('.depth-01').addEventListener('mouseleave', function(){
+        hideDropdownMenu(mainDropdownMenu);
+    });
+    btnMainMenu.addEventListener('click', function(){
+        if(this.classList.value === 'close'){
+            hideDropdownMenu(mainDropdownMenu);
+        }else{
+            showDropdownMenu(mainDropdownMenu);
+        }
+    });
+
+    mainDropdownMenuDepth01List.forEach((li, i, arr) => {
+        arr[i].addEventListener('mouseover',function(){
+            const current = i;
+            gnbBtns.forEach((btn, i, arr) => {
+                btn.classList.remove('on');
+                arr[current].classList.add('on');
+            });
+        });
+    });
+
     document.querySelector('#btn-search-toggle').addEventListener('click', function(){
-        console.log(searchArea.classList.value === 'on');
-        console.log(searchArea.classList.value);
         if(searchArea.classList.value === 'on'){
             hideSearchArea(searchArea);
         }else{
             showSearchArea(searchArea);
         }
+    });
+
+
+    // scroll 상태에 따른 Local Navigation Button Style
+    function setLnbStyle(e) {
+        if(e && getScrollDirection() === 'DOWN'){
+            actionHeaderToggle().hide();
+        } else if(e && getScrollDirection() === 'UP'){
+            actionHeaderToggle().show();
+        }
+    }
+    function actionHeaderToggle(){
+        if(document.documentElement.scrollTop >= window.innerHeight) {
+            return {
+                hide: function(){
+                    header.style['top'] = `-${header.offsetHeight}px`;
+                    header.style['transition'] = '1s';
+                },
+                show: function(){
+                    header.style['top'] = 0;
+                    header.style['transition'] = '1s';
+                }
+            };
+        }else{
+            return {
+                hide: function(){
+                    null;
+                },
+                show: function(){
+                    null;
+                }
+            };
+        }
+    }
+    document.addEventListener('scroll', function(e) {
+        setLnbStyle(e);
     });
 }())
